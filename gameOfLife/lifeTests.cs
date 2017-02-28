e22828e35ccad000a113533210dc667cc10e0784
TachyonJack <ysaxon@gmail.com>
2017-02-27 19:36:19 -0500

    cleaned up testfile

--- gameOfLife/lifeTests.cs
+++ gameOfLife/lifeTests.cs
@@ -1,11 +1,6 @@
-﻿using FsAlg.Generic;
-
-namespace gameOfLife
+﻿namespace gameOfLife
 {
 
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using FluentAssertions;
     using NUnit.Framework;
 
