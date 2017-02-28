8d74bf834c8351889059f9ba7b20b18139e21531
TachyonJack <ysaxon@gmail.com>
2017-02-27 19:35:01 -0500

    removed unused extension methods

--- gameOfLife/Interfaces.cs
+++ gameOfLife/Interfaces.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 using System.Dynamic;
 using System.Linq;
@@ -39,35 +38,5 @@ namespace gameOfLife
     public interface IBoardDisplay
     {
 
-    }
-    public static class Extensions
-    {
-        public static DataSet MultidimensionalArrayToDataSet(string[,] input)
-        {
-            var dataSet = new DataSet();
-            var dataTable = dataSet.Tables.Add();
-            var iFila = input.GetLongLength(0);
-            var iCol = input.GetLongLength(1);
-
-            //Fila
-            for (var f = 1; f < iFila; f++)
-            {
-                var row = dataTable.Rows.Add();
-                //Columna
-                for (var c = 0; c < iCol; c++)
-                {
-                    if (f == 1) dataTable.Columns.Add(input[0, c]);
-                    row[c] = input[f, c];
-                }
-            }
-            return dataSet;
-        }
-//        public static bool GetNextState(this ISquare square)
-//        {
-//            var aliveAround = square.Neighbors.AsParallel().Select(n => n.StateIsAlive).Count();
-//            return square.StateIsAlive
-//                ? Constants.StayAlive.Contains(aliveAround)
-//                : Constants.ComeAlive.Contains(aliveAround);
-//        }
     }
 }
---
+++ gameOfLife/lifeTests.cs
@@ -0,0 +1,39 @@
+﻿using FsAlg.Generic;
+
+namespace gameOfLife
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class LifeTests
+    {
+        private Board _mainBoard;
+
+        [SetUp]
+        public void Init()
+        {
+            _mainBoard = new Board(4, 3);
+            _mainBoard.SetPortionOfBoard(LifePatterns.Glider);
+        }
+
+        [Test]
+        public void NextState()
+        {
+            var gliderGen2 = new[,]
+            {
+                {false, false, false},
+                {true, false, true},
+                {false, true, true},
+                {false, true, false},
+            };
+            _mainBoard.NextState();
+            _mainBoard.GridSquaresAsBools.Should().Equal(gliderGen2);
+        }
+    }
+}
